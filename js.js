9ef6cc92ea44b88cc46b1665a58e4e6c61b8ced0
Daniel Boadu <mrboadu3@gmail.com>
2023-04-30 08:34:11 -0500

    added a css style , javascript DOM manipulation

--- js.js
+++ js.js
@@ -82,23 +82,79 @@ function play_round(player, computer_play){
 }
 
 
-function game(number_play){
-    let player_score= 0;
-    let computer_score= 0;
-    for(let i=0; i <= number_play; i++){
-        let results= play_round(player_selection("ROCK"),computer_choice());
-        if(results===win()){
-            player_score+=1
-        }else if(results===lose()){
-            computer_score+=1;
-        }
-    }
-    if(player_score > computer_score){
-        return "You won!!";
-    }else {
-       return "You lose!!";
-    }
-}
+
+const play_buttons= document.querySelector(".play_buttons");
+const paper_button= document.createElement("button");
+const rock_button= document.createElement("button");
+const scissors_button= document.createElement("button");
+play_buttons.appendChild(paper_button);
+play_buttons.appendChild(rock_button);
+play_buttons.append(scissors_button);
+
+paper_button.textContent= "PAPER";
+rock_button.textContent= "ROCK";
+scissors_button.textContent= "SCISSORS";
 
 
-console.log(game(10))
+const display_results= document.createElement("p");
+play_buttons.appendChild(display_results);
+
+
+
+let computer_score = 0;
+let player_score = 0;
+
+
+
+function updateScore() {
+    if (player_score === 5) {
+      display_results.textContent = "You won!!! Total score=" + player_score;
+      display_results.classList.remove('lose');
+      display_results.classList.add('win');
+      resetScores();
+    } else if (computer_score === 5) {
+      display_results.textContent = "You lose!!! Total score=" + computer_score;
+      display_results.classList.remove('win');
+      display_results.classList.add('lose');
+      resetScores();
+    } else {
+      display_results.textContent = "Your SCORE : " + player_score + " Computer SCORE: " + computer_score;
+      display_results.classList.remove('win', 'lose');
+    }
+  };
+  
+
+paper_button.addEventListener("click", function() {
+  if (play_round(player_selection("paper"), computer_choice()) === win()) {
+    player_score = player_score + 1;
+  } else if (play_round(player_selection("paper"), computer_choice()) === lose()) {
+    computer_score = computer_score + 1;
+  }
+  updateScore();
+});
+
+rock_button.addEventListener("click", function() {
+  if (play_round(player_selection("rock"), computer_choice()) === win()) {
+    player_score += 1;
+  } else if (play_round(player_selection("rock"), computer_choice()) === lose()) {
+    computer_score += 1;
+  }
+  updateScore();
+});
+
+scissors_button.addEventListener("click", function() {
+  if (play_round(player_selection("scissors"), computer_choice()) === win()) {
+    player_score += 1;
+  } else if (play_round(player_selection("scissors"), computer_choice()) === lose()) {
+    computer_score += 1;
+  }
+  updateScore();
+});
+
+// Reset scores if the game ends
+function resetScores() {
+  computer_score = 0;
+  player_score = 0;
+};
+
+
